6d41beee03d20b71a2063365eb8869a006518fe9
Fly me to the moon <git@bigly.dog>
2020-03-30 17:46:13 -0700

    emacs enable cua mode

--- init.el
+++ init.el
@@ -2,12 +2,22 @@
 ;;#################### Init Region ####################
 ;;#################### ########### ####################
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "extern" user-emacs-directory))
 
 
-;;#################### ################### ####################
-;;#################### Dependencies Region ####################
-;;#################### ################### ####################
+;;#################### ############### ####################
+;;#################### Internal Region ####################
+;;#################### ############### ####################
 (require 'init-elpa)
 (require 'init-misc)
 (require 'init-keyboard)
 (require 'init-mouse)
+
+
+;;#################### ############### ####################
+;;#################### External Region ####################
+;;#################### ############### ####################
+(require 'extern-undo)
+(require 'extern-helm)
+(require 'extern-tree)
+(require 'extern-tab)
