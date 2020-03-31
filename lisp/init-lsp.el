4744675d40453013c2cca0e6bbdbf962c38cf5e6
Fly me to the moon <git@bigly.dog>
2020-03-31 15:37:45 -0700

    isp

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -8,15 +8,17 @@
 
 (use-package company
   :ensure t
+  :command global-company-mode
+  :hook ((after-init . global-company-mode))
   :init (setq company-minimum-prefix-length 1
               company-idle-delay 0.0))
 
 
 (use-package lsp-mode
   :ensure t
-  :hook ((prog-mode-hook . lsp)
-        (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp)
+  :commands lsp
+  :hook ((prog-mode . lsp)
+        (lsp-mode . lsp-enable-which-key-integration)))
 
 
 (use-package lsp-ui
@@ -26,7 +28,9 @@
 
 (use-package company-lsp
   :ensure t
-  :commands company-lsp)
+  :commands company-lsp
+  :config
+  (push 'company-lsp company-backends))
 
 
 (use-package dap-mode
