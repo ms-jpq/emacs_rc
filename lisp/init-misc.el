56181c467d3138a4072abee9518ff80b998c6bcf
Fly me to the moon <git@bigly.dog>
2020-03-30 19:38:34 -0700

    more emacs settings

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -1,3 +1,12 @@
+;;#################### ########### ####################
+;;#################### Lang Region ####################
+;;#################### ########### ####################
+
+;; utf-8
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8-unix)
+
+
 ;;#################### ############## ####################
 ;;#################### Welcome Region ####################
 ;;#################### ############## ####################
@@ -15,11 +24,28 @@
 ;; watch filesystem for changes in open files
 (global-auto-revert-mode t)
 
+;; highlight current line
+(global-hl-line-mode 1)
+
 ;; line numbers
 (global-display-line-numbers-mode)
 
 ;; softwrap text
 (global-visual-line-mode t)
+
+;; tab-width
+(setq-default tab-width 2)
+
+
+;;#################### ########### ####################
+;;#################### Misc Region ####################
+;;#################### ########### ####################
+
+;; yes/no -> y/n
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; save minibuffer history
+(savehist-mode 1)
 
 
 ;;#################### END ####################
