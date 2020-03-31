97fd519f0b961c7af58bf1332e4bbea2ff0916a8
Fly me to the moon <git@bigly.dog>
2020-03-31 15:22:26 -0700

    setup helm

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -3,7 +3,17 @@
 ;;#################### ########### ####################
 
 (use-package helm
-  :ensure t)
+  :ensure t
+  :config
+  (helm-mode 1)
+  (define-key global-map [remap find-file] 'helm-find-files)
+  (define-key global-map [remap occur] 'helm-occur)
+  (define-key global-map [remap list-buffers] 'helm-buffers-list)
+  (define-key global-map [remap dabbrev-expand] 'helm-dabbrev)
+  (define-key global-map [remap execute-extended-command] 'helm-M-x)
+  (define-key global-map [remap apropos-command] 'helm-apropos)
+  (define-key lisp-interaction-mode-map [remap completion-at-point] 'helm-lisp-completion-at-point)
+  (define-key emacs-lisp-mode-map [remap completion-at-point] 'helm-lisp-completion-at-point))
 
 
 (use-package helm-lsp
