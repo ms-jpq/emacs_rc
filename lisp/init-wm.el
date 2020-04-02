a38c9d61703119f653d8837476e1fbff1e0eed76
Fly me to the moon <git@bigly.dog>
2020-04-02 00:44:04 -0700

    ibuffer sort by mode

--- lisp/init-wm.el
+++ lisp/init-wm.el
@@ -39,24 +39,14 @@
 ;; bring up ibuffer
 (bind-key "C-c u" 'ibuffer)
 
+;; ibuffer sort by mode
+(setq ibuffer-default-sorting-mode 'major-mode)
+
 ;; ibuffer auto refresh
 (add-hook
   'ibuffer-mode-hook
   (lambda ()
     (ibuffer-auto-mode t)))
-
-;; (ibuffer-switch-to-saved-filter-groups "default")
-
-;; organize ibuffer into groups
-;; (setq ibuffer-saved-filter-groups
-;;   '(("default"
-;;     ("emacs" (or (name . "^\\*scratch\\*$")
-;;                  (name . "^\\*Messages\\*$"))
-;;     ("dired" (mode . dired-mode))
-;;     ("shell" (or (mode . eshell-mode)
-;;                  (mode . shell-mode)))
-;;     ("magit" (mode . magit-mode))
-;;     ("code" (mode . prog-mode))))))
 
 
 ;;#################### END ####################
