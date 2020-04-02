6270fca8a5adf7577978307587743112f03bf62e
Fly me to the moon <git@bigly.dog>
2020-04-01 23:22:14 -0700

    custom buf hotky

--- lisp/init-wm.el
+++ lisp/init-wm.el
@@ -21,20 +21,30 @@
   (tabbar-mode t))
 
 
-;; make popup window easier to dismiss
+;; make popup panes easier to dismiss
 (use-package popwin
   :ensure t
   :config
   (popwin-mode t))
 
 
-;; move buffers around
+;; move panes around
 (use-package buffer-move
   :ensure t
   :bind (("C-c <up>" . buf-move-up)
          ("C-c <down>" . buf-move-down)
          ("C-c <left>" . buf-move-left)
          ("C-c <right>" . buf-move-right)))
+
+
+;;#################### ############## ####################
+;;#################### Buffers Region ####################
+;;#################### ############## ####################
+
+;; bring up ibuffer
+(bind-key "C-c u" 'ibuffer)
+
+
 
 
 ;;#################### END ####################
