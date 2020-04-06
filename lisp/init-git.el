3816aad592ac4a4cc76b03e02085946992f7d673
Fly me to the moon <git@bigly.dog>
2020-04-06 00:43:39 -0700

    magit mode has consistent hotkey

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -4,7 +4,9 @@
 
 (use-package magit
   :ensure t
-  :bind (("C-c C-m" . magit))
+  :bind (("C-c C-m" . magit)
+         :map magit-mode-map
+              ("C-q" . magit-mode-bury-buffer))
   :config
   (global-auto-revert-mode t))
 
