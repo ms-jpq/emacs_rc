e9e77e1a1e549be73b8dd2fa2fe0ddd55bcbb1ae
Fly me to the moon <git@bigly.dog>
2020-04-07 03:53:57 -0700

    ivy xref

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -75,5 +75,19 @@
               ("C-s" . swiper)))
 
 
+;; use ivy in xref TODO: remove emacs <27
+(use-package ivy-xref
+  :ensure t
+  :init
+  ;; xref initialization is different in Emacs 27 - there are two different
+  ;; variables which can be set rather than just one
+  (when (>= emacs-major-version 27)
+    (setq xref-show-definitions-function #'ivy-xref-show-defs))
+  ;; Necessary in Emacs <27. In Emacs 27 it will affect all xref-based
+  ;; commands other than xref-find-definitions (e.g. project-find-regexp)
+  ;; as well
+  (setq xref-show-xrefs-function #'ivy-xref-show-xrefs))
+
+
 ;;#################### END ####################
 (provide 'init-ivy)
