f8be17f6479419d302b5fc3327d89d36afaf100b
Fly me to the moon <git@bigly.dog>
2020-04-19 23:17:17 -0700

    swiper solves everything

--- lisp/editor/init-search.el
+++ lisp/editor/init-search.el
@@ -14,22 +14,7 @@
                    (let ((selection (current-selection)))
                      (when selection
                        (deactivate-mark))
-                     (swiper selection))))
-              :map swiper-map
-              ("C-o" . swiper-query-replace)))
-
-
-;; writable ivy occur buffer
-(use-package wgrep
-  :defer
-  :custom
-  (wgrep-auto-save-buffer t)
-  :bind (:map ivy-occur-grep-mode-map
-              ("w" . nil)
-              ("C-o" . ivy-wgrep-change-to-wgrep-mode)
-              :map wgrep-mode-map
-              ("C-s" . wgrep-finish-edit)
-              ("C-k" . wgrep-abort-changes)))
+                     (swiper selection))))))
 
 
 ;;#################### END ####################
